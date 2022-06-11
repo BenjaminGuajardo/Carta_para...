e0219c4fe2fc91803f6b06307dd44f2fe47d2b2f
BenjaminGuajardo <106709123+BenjaminGuajardo@users.noreply.github.com>
2022-06-11 02:34:54 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+alert("Hola");
+var nombre = prompt("¿Cómo te llamas? (No hagas trampa xD)");
+alert("Hola " + nombre + ", espero que te encuentres bien");
+alert("Yo me llamo Alerta de Js, estoy programado para recibirte");
+alert("Mi creador ha hecho algo especial para ti");
+alert("No sé si sea de tu agrado, pero te ha hecho esto");
+prompt("Responde 'Sí' si deseas verlo, si respondes 'No' lo siento igual te lo enseñaré");
